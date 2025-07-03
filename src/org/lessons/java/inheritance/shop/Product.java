94a43c7c657f790668eeb73df78cd43e8d34b022
vmqxdv <vquintilio@gmail.com>
2025-07-03 11:58:00 +0200

    feat: Implement constructors for Product

--- src/org/lessons/java/inheritance/shop/Product.java
+++ src/org/lessons/java/inheritance/shop/Product.java
@@ -1,6 +1,7 @@
 package org.lessons.java.inheritance.shop;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
 public class Product {
 
@@ -9,4 +10,63 @@ public class Product {
   private String brand;
   private BigDecimal price;
   private BigDecimal iva;
+
+  public Product() {
+    this.code = 0;
+    this.name = "";
+    this.brand = "";
+    this.price = new BigDecimal(0);
+    this.iva = new BigDecimal(0);
+  }
+
+  public Product(String name, String brand, BigDecimal price, BigDecimal iva) {
+    Random random = new Random();
+
+    this.code = random.nextInt(999999);
+    this.name = name;
+    this.brand = brand;
+    this.price = price;
+    this.iva = iva;
+  }
+
+  // -- Code
+  public int getCode() {
+    return this.code;
+  }
+
+  // -- Name
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  // -- Brand
+  public String getBrand() {
+    return this.brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  // -- Price
+  public BigDecimal getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  // -- Iva
+  public BigDecimal getIva() {
+    return this.iva;
+  }
+
+  public void setIva(BigDecimal iva) {
+    this.iva = iva;
+  }
 }
