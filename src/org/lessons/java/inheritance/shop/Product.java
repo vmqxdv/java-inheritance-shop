3927740a43b4f04c7862bfe947b46a21b33db26e
vmqxdv <vquintilio@gmail.com>
2025-07-03 11:42:49 +0200

    feat: Added basic fiends to the superclass "Product"

--- src/org/lessons/java/inheritance/shop/Product.java
+++ src/org/lessons/java/inheritance/shop/Product.java
@@ -1,5 +1,12 @@
 package org.lessons.java.inheritance.shop;
 
+import java.math.BigDecimal;
+
 public class Product {
 
+  int code;
+  String name;
+  String brand;
+  BigDecimal price;
+  BigDecimal iva;
 }
