5394689ab6c59a5632dddf7a95d7038ffea39728
vmqxdv <vquintilio@gmail.com>
2025-07-03 13:03:21 +0200

    test: Testing override

--- src/org/lessons/java/inheritance/shop/Main.java
+++ src/org/lessons/java/inheritance/shop/Main.java
@@ -8,5 +8,10 @@ public class Main {
     Smartphone smartphone = new Smartphone("Iphone 16", "Apple", new BigDecimal(1599.99), new BigDecimal(0.22), 512f);
 
     System.out.println(smartphone.toString());
+
+    Television television = new Television("X1234G4K", "Samsung", new BigDecimal(1998.65), new BigDecimal(0.22), 100,
+        true);
+
+    System.out.println(television.toString());
   }
 }
